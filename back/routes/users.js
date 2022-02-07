fb3f8f1d4cc999fdd7fbe0b2948c47f6718a6c71
ShuraShiraki <shuinfsince@gmail.com>
2022-02-07 14:53:49 +0900

    jsonファイルの読み取り

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+var test_users_list = {
+  "1": require('../test/users/test1.json'),
+  "2": require('../test/users/test2.json'),
+  "3": require('../test/users/test3.json'), 
+  "4": require('../test/users/test4.json'),
+  "5": require('../test/users/test5.json')
+}
+*/
 
 var sample_users = {
   "data": [    {
+      "id": "1",
       "name": "田中太郎",
       "books": ["Githubについて", "アジャイル開発について"],
       "skils": ["Github", "アジャイル"]
     }]
 };
 
-/* GET home page. */
+/* GET Users */
 router.get('/', function(req, res, next) {
   res.json(sample_users);
 });
+
+
+/* GET Test Users */
+router.get('/:test', function(req, res, next) {
+  var test_users = test_users_list[req.params.test]
+  res.json(test_users);
+});
+
 
 module.exports = router;
