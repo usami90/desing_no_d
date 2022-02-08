b5f3c8aa0ebf638854923317e5b9d8231e4a5681
ShuraShiraki <shuinfsince@gmail.com>
2022-02-08 15:41:19 +0900

    SQLの編集

--- back/compose/mysql/sql/01_DB_DDL.sql
+++ back/compose/mysql/sql/01_DB_DDL.sql
@@ -4,17 +4,17 @@ DROP DATABASE IF EXISTS `library`;
 CREATE DATABASE `library` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE library.user(
-    user_id VARCHAR(4) NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_name VARCHAR(100) NOT NULL,
-    book VARCHAR(1000),
-    skill VARCHAR(1000)
+    books VARCHAR(1000),
+    skills VARCHAR(1000)
 );
 
-INSERT INTO library.user ( user_id, user_name, book, skill )
+INSERT INTO library.user ( user_id, user_name, books, skills )
 VALUES (
     '0001',
     '田中太郎',
-    'アジャイル開発について,Githubについて',
+    'アジャイル開発について:良かった,Githubについて:初心者向け',
     'アジャイル,Github'
 );
 
