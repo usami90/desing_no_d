c6e176a84042d9990c939b05d40eac767e3ec21c
有江 禎晶 <arie.yoshiaki.sh8@jp.nssol.nipponsteel.com>
2022-02-08 18:12:30 +0900

    add test data sql

---
+++ back/test/users/all_users_insert.sql
@@ -0,0 +1,43 @@
+delete from library.user;
+
+INSERT INTO library.user ( user_name, books, skills )
+VALUES (
+    '田中太郎',
+    'アジャイル開発について:良かった,Githubについて:初心者向け,WF開発について:PMO上級向け',
+    'アジャイル,Github'
+);
+
+INSERT INTO library.user ( user_name, books, skills )
+VALUES (
+    '田中次郎',
+    'オラクルについて:初心者向け',
+    'アジャイル'
+);
+
+INSERT INTO library.user ( user_name, books, skills )
+VALUES (
+    '鈴木三郎',
+    'オラクルについて:',
+    ''
+);
+
+INSERT INTO library.user ( user_name, books, skills )
+VALUES (
+    '鈴木四郎',
+    '',
+    'WF'
+);
+
+INSERT INTO library.user ( user_name, books, skills )
+VALUES (
+    '中央空文字データ',
+    'アジャイル開発について:良かった,:,WF開発について:PMO上級向け',
+    'アジャイル,Github'
+);
+
+INSERT INTO library.user ( user_name, books, skills )
+VALUES (
+    'コメントが長いデータ',
+    'アジャイル開発について:良かった,:★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入★テスト文字列で長い文章を記入,WF開発について:PMO上級向け',
+    'アジャイル,Github'
+);
