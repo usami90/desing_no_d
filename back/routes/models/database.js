c9196d291ce38a1303f250f1b0d63dc4866fe38b
ShuraShiraki <shuinfsince@gmail.com>
2022-02-08 11:40:20 +0900

    DB接続の実装

---
+++ back/routes/models/database.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2');
+
+/* MySQL の接続設定 */
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'passwd',
+  database: 'library',
+  port: '3306'
+});
+
+/* MySQLへの接続 */
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connection Success');
+});
+
+
+/* SQL文を引数として、コマンドを実行する */
+function doQuery(sql){
+  return new Promise((resolve, result) => {
+    connection.query(sql, (err, result) => {
+      return resolve(result);
+    })
+  }).catch('error', (err) =>  reject(err))
+}
+
+module.exports = {
+  doQuery:doQuery
+}
