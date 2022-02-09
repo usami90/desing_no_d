6f688080e44e8f51b7191afd32d675772bb6bd84
usami90 <usami.masaharu.t5p@jp.nssol.nipponsteel.com>
2022-02-09 14:23:01 +0900

    スキルテーブル登録DDL作成.スキルテーブル用テストデータ作成

---
+++ sql/all_skills_insert.sql
@@ -0,0 +1,14 @@
+delete from library.skills;
+
+INSERT INTO library.skills (name) VALUES
+('アジャイル'),
+('ウォーターフォール'),
+('Github'),
+('Java'),
+('C++'),
+('C#'),
+('JavaScript'),
+('PHP'),
+('オラクル'),
+('MySQL'),
+('PostgreSQL');
