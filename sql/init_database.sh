101cb24af769168465d9ba7b3caaf091144c17f6
ShuraShiraki <shuinfsince@gmail.com>
2022-02-09 17:29:57 +0900

    DB初期化のshファイルの編集

--- sql/init_database.sh
+++ sql/init_database.sh
@@ -1,2 +1,4 @@
 #run the setup script to create the DB and the schema in the DB
 mysql -u docker -pdocker test_database < "/docker-entrypoint-initdb.d/01_DB_DDL.sql"
+mysql -u docker -pdocker test_database < "/docker-entrypoint-initdb.d/all_users_insert.sql"
+mysql -u docker -pdocker test_database < "/docker-entrypoint-initdb.d/all_skills_insert.sql"
