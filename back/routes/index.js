b37a03c8b7b08f9cdaa644f326c2943e6a6191f6
ShuraShiraki <shuinfsince@gmail.com>
2022-02-09 15:45:08 +0900

    コードの整形

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,14 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 var db = require("./models/database");
-var sample_skills = 
-   [
-   { "id":1, "name": "アジャイル"},
-  { "id":2, "name": "Github"},
-  { "id":3, "name": "オラクル"},
-  { "id":4, "name": "デザイン"}
-  ];
-  
+var sample_skills = [
+  { id: 1, name: "アジャイル" },
+  { id: 2, name: "Github" },
+  { id: 3, name: "オラクル" },
+  { id: 4, name: "デザイン" },
+];
 
 /* GET Users from DB */
 router.get("/", (req, res, next) => {
@@ -17,22 +15,22 @@ router.get("/", (req, res, next) => {
   (async () => {
     var sql = "SELECT * FROM user;";
     var result = await db.doQuery(sql);
-    var users = result.map( (user) => {
-      book = user.books.split(",").map( (book) => {
+    var users = result.map((user) => {
+      book = user.books.split(",").map((book) => {
         bookinfo = book.split(":");
         return {
-          "title": bookinfo[0],
-          "comment": bookinfo[1]
-        }
-      })
+          title: bookinfo[0],
+          comment: bookinfo[1],
+        };
+      });
       return {
-        "userid": user.user_id,
-        "name": user.user_name,
-        "books": book,
-        "skills": user.skills.split(",")
-      }
-    })
-    res.json({"data": {"users":users,"skills": sample_skills}});
+        userid: user.user_id,
+        name: user.user_name,
+        books: book,
+        skills: user.skills.split(","),
+      };
+    });
+    res.json({ data: { users: users, skills: sample_skills } });
   })().catch(next);
 });
 
--- back/routes/users.js
+++ back/routes/users.js
@@ -1,46 +1,32 @@
-var express = require("express");
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
-var db = require("./models/database");
+// var db = require("./models/database");
 
-var test_users_list = {
-  1: require("../test/users/test1.json"),
-  2: require("../test/users/test2.json"),
-  3: require("../test/users/test3.json"),
-  4: require("../test/users/test4.json"),
-  5: require("../test/users/test5.json"),
-};
+// /* GET Users from DB */
+// router.get("/", (req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+//   (async () => {
+//     var sql = "SELECT * FROM user;";
+//     var result = await db.doQuery(sql);
+//     var data = result.map( (user) => {
+//       book = user.books.split(",").map( (book) => {
+//         bookinfo = book.split(":");
+//         return {
+//           "title": bookinfo[0],
+//           "comment": bookinfo[1]
+//         }
+//       })
+//       return {
+//         "userid": user.user_id,
+//         "name": user.user_name,
+//         "books": book,
+//         "skills": user.skills.split(",")
+//       }
+//     })
+//     res.json({ "data": data });
+//   })().catch(next);
+// });
 
-/* GET Users from DB */
-router.get("/", (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-  (async () => {
-    var sql = "SELECT * FROM user;";
-    var result = await db.doQuery(sql);
-    var data = result.map( (user) => {
-      book = user.books.split(",").map( (book) => {
-        bookinfo = book.split(":");
-        return {
-          "title": bookinfo[0],
-          "comment": bookinfo[1]
-        }
-      })
-      return {
-        "userid": user.user_id,
-        "name": user.user_name,
-        "books": book,
-        "skills": user.skills.split(",")
-      }
-    })
-    res.json({ "data": data });
-  })().catch(next);
-});
 
-/* GET Test Users */
-router.get("/:test", function (req, res, next) {
-  var test_users = test_users_list[req.params.test];
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.json(test_users);
-});
-
-module.exports = router;
+// module.exports = router;
