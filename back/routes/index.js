a33e6e96197df754c750ab920c6c02eaddfbfac0
ShuraShiraki <shuinfsince@gmail.com>
2022-02-09 16:47:13 +0900

    コードの整形

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,15 +1,14 @@
 var express = require("express");
-const { doQuery } = require("./models/database");
 var router = express.Router();
 
-var db = require("./models/database");
+const { doQuery } = require("./models/database");
 
 /* GET Users and Skills from DB */
 router.get("/", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   (async () => {
     var user_sql = "SELECT * FROM user;";
-    var user_result = await db.doQuery(user_sql);
+    var user_result = await doQuery(user_sql);
     var users = user_result.map((user) => {
       book = user.books.split(",").map((book) => {
         bookinfo = book.split(":");
