ab13c570e3b19d8209560f16317612d1b27da85e
ShuraShiraki <shuinfsince@gmail.com>
2022-02-15 10:43:50 +0900

    プレゼンター決め

---
+++ front/src/Presentor.vue
@@ -0,0 +1,33 @@
+<template>
+  <div align="center">
+    <button
+      style="width: 300px; padding: 20px; font-size: 180%"
+      v-on:click="randomMember()"
+    >
+      本日の発表者
+    </button>
+    <p><font size="6" color="#ee0000">{{ this.pm }}</font></p>
+  </div>
+</template>
+
+<script>
+// Webページ上のデータや操作の埋込
+export default {
+  // データの設定
+  data() {
+    return {
+      members: ["アリエル", "アリエッティ", "有江さん", "宇佐神さん", "陳さん", "白木さん"],
+      pm: "",
+    };
+  },
+
+  // メソッド
+  methods: {
+    randomMember() {
+      var m = this.members[Math.floor(Math.random() * 6)];
+      this.pm = "本日の発表者は、" + m + " です。";
+      alert(this.pm);
+    },
+  },
+};
+</script>
--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Users from './Users.vue'
+import Presentor from './Presentor.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(Users),
 }).$mount('#users')
+
+new Vue({
+  render: h => h(Presentor),
+}).$mount('#presentor')
